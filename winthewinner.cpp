bf925c01e681043051c720b8ff52737029aff0ef
Mostofa Hasin Mahdi <102316142+Mostofa-Hasin-Mahdi@users.noreply.github.com>
2023-07-15 21:05:06 +0600

    Add files via upload

---
+++ winthewinner.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    string s,m;
+    cin>>s>>m;
+    cout<<"Congratulations, The Winner Is: "<<m<<endl;
+    return 0;
+
+}
