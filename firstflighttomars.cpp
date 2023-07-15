2f6cd5787142ccbd994eb75d22da7a4afbfeed93
Mostofa Hasin Mahdi <102316142+Mostofa-Hasin-Mahdi@users.noreply.github.com>
2023-07-15 21:00:46 +0600

    Add files via upload

---
+++ firstflighttomars.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    string s;
+    cin>>s;
+    if(s.size()>10)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+}
